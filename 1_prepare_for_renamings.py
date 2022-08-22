a0dceb47b1b5ef1d55b3b32202a5a71c3b8411a3
Vladimír Ulman <ulman@fi.muni.cz>
2022-08-22 16:05:36 +0200

    tabs -> 4spaces

--- 1_prepare_for_renamings.py
+++ 1_prepare_for_renamings.py
@@ -17,27 +17,27 @@ renamingFile = wrkDirStr + os.path.sep + renameFileName
 # fetch the list of all files in the wrkDirStr directory
 allFiles = ""
 for rootDir,dirs,files in os.walk(wrkDirStr, topdown = True):
-	#print(f"root = {rootDir}, dirs={dirs}, files={files}")
-	allFiles = files
-	break
+    #print(f"root = {rootDir}, dirs={dirs}, files={files}")
+    allFiles = files
+    break
 #print(f"all detected files={allFiles}")
 
 # reduce the list to only wanted files, then extract all observed middle sections
 middleSections = set()
 for file in allFiles:
-	if not file.endswith(patternWhatFilesToCareAboutOnly):
-		continue
-	midIdx = file.find(patternHowMiddleSectionStarts)
-	lstIdx = file.find(patternHowLastSectionStarts)
-	# were the sections detected at all?
-	if midIdx > -1 and lstIdx > midIdx:
-		midStr = file[midIdx:lstIdx]
-		middleSections.add(midStr)
+    if not file.endswith(patternWhatFilesToCareAboutOnly):
+        continue
+    midIdx = file.find(patternHowMiddleSectionStarts)
+    lstIdx = file.find(patternHowLastSectionStarts)
+    # were the sections detected at all?
+    if midIdx > -1 and lstIdx > midIdx:
+        midStr = file[midIdx:lstIdx]
+        middleSections.add(midStr)
 print("all observed middle sections:")
 print(middleSections)
 
 outFile = open(renamingFile,"w")
 for m in sorted(middleSections):
-	outFile.write(m + " -> " + m + "\n")
+    outFile.write(m + " -> " + m + "\n")
 outFile.close()
 print("were written into a file: " + renamingFile)
--- 2_rename_files.py
+++ 2_rename_files.py
@@ -22,49 +22,49 @@ renamingFile = wrkDirStr + os.path.sep + renameFileName
 renameMap = dict()
 mapFile = open(renamingFile,"r")
 for pair in mapFile:
-	sepIdx = pair.find("->")
-	if sepIdx == -1:
-		continue
-	fromStr = pair[0:sepIdx].strip()
-	toStr = pair[sepIdx+2:].strip()
-	renameMap[fromStr] = toStr
+    sepIdx = pair.find("->")
+    if sepIdx == -1:
+        continue
+    fromStr = pair[0:sepIdx].strip()
+    toStr = pair[sepIdx+2:].strip()
+    renameMap[fromStr] = toStr
 mapFile.close()
 print("considering the following renaming map:")
 for m in renameMap:
-	print(m+" -> "+renameMap[m])
+    print(m+" -> "+renameMap[m])
 print("")
 
 # fetch the list of all files in the wrkDirStr directory
 allFiles = ""
 for rootDir,dirs,files in os.walk(wrkDirStr, topdown = True):
-	#print(f"root = {rootDir}, dirs={dirs}, files={files}")
-	allFiles = files
-	break
+    #print(f"root = {rootDir}, dirs={dirs}, files={files}")
+    allFiles = files
+    break
 #print(f"all detected files={allFiles}")
 
 # reduce the list to only wanted files, then do the renaming
 for file in allFiles:
-	if not file.endswith(patternWhatFilesToCareAboutOnly):
-		continue
-	midIdx = file.find(patternHowMiddleSectionStarts)
-	lstIdx = file.find(patternHowLastSectionStarts)
-	# were the sections detected at all?
-	if midIdx == -1 or lstIdx <= midIdx:
-		print("Warning: " + file + " with not explicit middle section was skipped!")
-		continue
-	midStr = file[midIdx:lstIdx]
-	newMidStr = renameMap.get(midStr,None)
-	if newMidStr is None:
-		print("Warning: " + file + " with unknown middle section " + midStr + " was skipped!")
-		continue
+    if not file.endswith(patternWhatFilesToCareAboutOnly):
+        continue
+    midIdx = file.find(patternHowMiddleSectionStarts)
+    lstIdx = file.find(patternHowLastSectionStarts)
+    # were the sections detected at all?
+    if midIdx == -1 or lstIdx <= midIdx:
+        print("Warning: " + file + " with not explicit middle section was skipped!")
+        continue
+    midStr = file[midIdx:lstIdx]
+    newMidStr = renameMap.get(midStr,None)
+    if newMidStr is None:
+        print("Warning: " + file + " with unknown middle section " + midStr + " was skipped!")
+        continue
 
-	newFile = file[0:midIdx] + newMidStr + file[lstIdx:]
-	if not dryRun:
-		print("renaming: "+file+" -> "+newFile)
+    newFile = file[0:midIdx] + newMidStr + file[lstIdx:]
+    if not dryRun:
+        print("renaming: "+file+" -> "+newFile)
 
-	inFile  = wrkDirStr + os.path.sep + file
-	outFile = outDirStr + os.path.sep + newFile
-	if dryRun:
-		print("NOT moving "+inFile+" to "+outFile)
-	else:
-		shutil.move(inFile,outFile)
+    inFile  = wrkDirStr + os.path.sep + file
+    outFile = outDirStr + os.path.sep + newFile
+    if dryRun:
+        print("NOT moving "+inFile+" to "+outFile)
+    else:
+        shutil.move(inFile,outFile)
